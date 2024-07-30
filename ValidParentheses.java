aafe88dae6856c845d99df56c61d4bbf00996956
Your Name <kgupta.abps@gmail.com>
2024-07-30 23:23:45 +0530

    DSA Day 3

---
+++ ValidParentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } 
+            else {
+                if (stack.isEmpty() || !isMatchingPair(stack.pop(), c)) {
+                    return false; // Invalid case
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+
+    private boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    }
+
+    }

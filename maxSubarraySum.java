f80d3fc54c0402b700ea8f77c637a7c39cf204ea
Kashika Gupta <93068969+kashikaga@users.noreply.github.com>
2025-04-05 08:33:26 +0530

    maxSubarraySum using kadanes

--- maxSubarraySum.java
+++ maxSubarraySum.java
@@ -61,3 +61,26 @@ int numbers[]={1,-2,6,-1,3};
 maxSubarraySum(numbers);
 }
 }
+
+//kadanes
+import java.util.*;
+public class kadanes{
+public static void kadanes(int numbers[]){
+int cs=0;
+int ms=Integer.MIN_VALUE;
+
+for(int i=0;i<numbers.length;i++){
+cs=cs+numbers[i];
+if(cs<0){
+cs=0;
+}
+ms=Math.max(cs,ms);
+}
+System.out.println("max subarray Sum="+ ms);
+}
+
+public static void main(String args[]){
+int numbers[]={-2,-3,4,-1,-2,1,5,-3};
+kadanes(numbers);
+}
+}

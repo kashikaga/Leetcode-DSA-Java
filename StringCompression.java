054009eb59ee5c5b64b287e976f07fa157879902
Kashika Gupta <93068969+kashikaga@users.noreply.github.com>
2025-04-09 14:24:47 +0530

    Time Complexity: O(n)

--- StringCompression.java
+++ StringCompression.java
@@ -1,32 +1,26 @@
+//https://leetcode.com/problems/string-compression/
+
 class Solution {
     public int compress(char[] chars) {
-         int writeIndex = 0;  // Index to write compressed characters
-        int readIndex = 0;   // Index to read characters from the input array
-
-        while (readIndex < chars.length) {
-            char currentChar = chars[readIndex];
+        int n = chars.length;
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = chars[i];
             int count = 0;
-
-            // Count the number of occurrences of the current character
-            while (readIndex < chars.length && chars[readIndex] == currentChar) {
-                readIndex++;
+            while (i < n && chars[i] == ch) {
                 count++;
+                i++;
             }
-
-            // Write the current character to the output
-            chars[writeIndex++] = currentChar;
-
-            // If count is greater than 1, write the count as well
-            if (count > 1) {
-                // Convert count to characters and write them to the array
-                for (char digit : String.valueOf(count).toCharArray()) {
-                    chars[writeIndex++] = digit;
+            if (count == 1) {
+                chars[idx++] = ch;
+            } else {
+                chars[idx++] = ch;
+                for (char digit : Integer.toString(count).toCharArray()) {
+                    chars[idx++] = digit;
                 }
             }
+            i--;
         }
-
-        // Return the new length of the compressed array
-        return writeIndex;
-
+        return idx;
     }
 }
